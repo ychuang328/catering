0aa763a7caca254a89aa8ba47cfd3d0b159fbe82
ychuang328 <778732616@qq.com>
2016-08-10 23:13:58 +0800

    create sub project
    Signed-off-by: ychuang328 <778732616@qq.com>

---
+++ catering-service/src/main/java/com/weiwork/catering/service/IUserService.java
@@ -0,0 +1,19 @@
+/**
+ * IUserService.java
+ * com.weiwork.catering.service
+ * Copyright (c) 2016, 北京名雅轩版权所有.
+*/
+
+package com.weiwork.catering.service;
+
+/**
+ * TODO(这里用一句话描述这个类的作用)
+ * <p>
+ * TODO(这里描述这个类补充说明 – 可选)
+ * @author   daya328
+ * @Date	 2016-8-10 	 
+ */
+public class IUserService {
+
+}
+
